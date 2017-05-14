15eb81b0066c6309eba02ad844f582728df2625d
nguyenduongy <duongynguyenit@gmail.com>
2017-05-14 23:43:55 +0700

    Complete

--- TinhCanChi/Program.cs
+++ TinhCanChi/Program.cs
@@ -10,6 +10,17 @@ namespace TinhCanChi
     {
         static void Main(string[] args)
         {
+            int year;
+            string[] Can = { "Canh", "Tan", "Nham", "Quy", "Giap", "At", "Binh", "Dinh", "Mau", "Ky" };
+
+            string[] Chi = { "Than", "Dau", "Tuat", "Hoi", "Ty", "Suu", "Dan", "Meo", "Thin", "Ty", "Ngo", "Mui" };
+
+            Console.WriteLine("Moi ban nhap mot nam bat ky: ");
+
+            year = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nam {0} co nam am lich la: {1} {2}", year, Can[year % 10], Chi[year % 12]);
+
             Console.ReadLine();
         }
     }
